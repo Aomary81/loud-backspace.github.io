54423c263a98bfd5f673bb0b88aeb02484c6b2ed
Peter <emaaba35@gmail.com>
2022-11-01 16:03:43 -0700

    convert static html, css and js to react so they run with npm start

---
+++ rezidenc-app/src/App.js
@@ -0,0 +1,11 @@
+import Home from './pages/home'; 
+import './App.css';
+
+//var perf = require('./landing-page-files/index.html');
+
+function App() {
+  return (
+    <Home />
+  );
+}
+export default App;
