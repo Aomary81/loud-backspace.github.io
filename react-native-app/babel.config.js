ef9a62d431af97ba32304dcce3092401cfd55a56
Kenneth Munk <contact.me@kenmunk.com>
2022-11-14 00:56:13 -0800

    LB-84 -- Boiler-plate sidebar with buttons going to different pages in the app
    Changes:
    + Babel config modified to support react navigation animations
    + additional node modules installed to the project modifying the package.json with additional package requirements/dependencies (react-navigation /react-native-vector-icons)
    + Content layout modified to include new sidebar content with dynamic layout allowing for expandability
    +ExpandableIcon function component created to simplify the process of creating a dynamic sidebar
    +Page layouts modified to support vertical centering of content in userIconTitle
    +ContentArea function component created to streamline the content display process

--- react-native-app/babel.config.js
+++ react-native-app/babel.config.js
@@ -1,16 +1,17 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-	"plugins": [
-    [
-      "module-resolver",
-      {
-        "alias": {
-          "@PageLayouts": "./pages/components/PageLayouts"
-        },
-      }
-    ]
-  ]
-  };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'./node_modules/react-native-reanimated/plugin',
+			[
+				"module-resolver",
+				{
+					"alias": {
+						"@PageLayouts": "./pages/components/PageLayouts"
+					},
+				}
+			]
+		]
+	};
 };
