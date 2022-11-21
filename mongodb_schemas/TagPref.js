42e9248890eb1a9a3af5af0337d9fb1db7c2fe07
Kenneth Munk <contact.me@kenmunk.com>
2022-11-20 20:37:31 -0800

    LB-86 -- Create schema for user rating (#10)
    Schema for user ratings imported
    Base tags for selection
    Derived tags used in conjunction with person and reviews

---
+++ mongodb_schemas/TagPref.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose);
+
+const TagPrefSchema = new mongoose.Schema({
+	
+	//userRef: {type: Schema.Types.ObjectId, ref: "User"}
+	tagRef: {type: Schema.Types.ObjectId, ref: "Tag"},
+	score: Number
+	
+});
