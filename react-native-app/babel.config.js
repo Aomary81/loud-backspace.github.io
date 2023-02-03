fe0518c2b1d81e67467ea2f808229c27d7fccf4f
Kenneth Munk <da.munkk@gmail.com>
2023-02-02 22:24:10 -0800

    LB-123 Create Content Base Column
    ~ Modified babel config to include disclaimer that use of the alias system will not work for destination directories
    + created the Content Base Column with some documentation on usage

--- react-native-app/babel.config.js
+++ react-native-app/babel.config.js
@@ -7,9 +7,12 @@ module.exports = function(api) {
 			[
 				"module-resolver",
 				{
+					"root": ["./pages"],
 					"alias": {
 						"@PageLayouts": "./pages/components/PageLayouts"
 					},
+					//Paths put into alias must go all of the way to the destination file
+					//Experimentation has found that aliases do not work well for destination directories
 				}
 			]
 		]
