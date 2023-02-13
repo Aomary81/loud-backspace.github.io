89a01bcadc80ad55327b629e981f2ef02a641491
Andriy Storozhuk <dreystorz@yahoo.com>
2023-02-12 20:30:10 -0800

    LB-127 Create content module

---
+++ react-native-app/pages/components/V2Components/ContentModule.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {View, Platform} from 'react-native';
+
+/**
+ContentModules hold content and are placed in TitleView. When using specify the height and width props.
+
+//*/
+const isWeb = Platform.OS === 'web';
+export default function ContentModule(props){
+	
+	return (
+		<View style={
+			Object.assign
+			(
+				{
+					alignItems: 'center',
+                    justifyContent: 'center',
+					width: props.width,
+                    height: props.height,
+                    backgroundColor: '#E1E1E1',
+                    margin: 10,
+                    borderRadius: 10,
+				}, 
+				props.style
+			)
+		}>
+			{props.children}
+		</View>
+	);
+}
