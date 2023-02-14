7a38bc2e9a1e51f08e5e1aac77385859fcdd7a37
Aomary81 <89872670+Aomary81@users.noreply.github.com>
2023-02-14 13:34:44 -0800

    LB-34  view and edit my own information

---
+++ react-native-app/pages/AccountNavigation/AccountNavigation.js
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import AccountCreation from "./AccountEditing";
+
+const Stack = createNativeStackNavigator();
+
+function AccountNavigation() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="AccountEditing" component={AccountCreation} />
+    </Stack.Navigator>
+  );
+}
+
+export default AccountNavigation;
