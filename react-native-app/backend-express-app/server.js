99330862e50c1c69be358ea860aa054687caa8c3
Peter <emaaba35@gmail.com>
2023-02-17 18:45:59 -0800

    LB-66 completed, mongodb is now connected and talking to express backend.

--- react-native-app/backend-express-app/server.js
+++ react-native-app/backend-express-app/server.js
@@ -1,13 +1,26 @@
-const express = require( 'express');
-const cors = require( 'cors');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require ('dotenv').config;
+require('dotenv').config();
 
 const app = express ();
 const port = process.env.PORT || 3000;
 
 app.use (cors());
 app.use (express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
 
 app.listen (port, () => {
     console. log (`Server is running on port: ${port}`);
