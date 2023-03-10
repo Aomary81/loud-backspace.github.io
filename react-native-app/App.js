c56570964973a75f25a66db39aa0bd077758596d
Andriy Storozhuk <dreystorz@yahoo.com>
2023-03-09 21:31:25 -0800

    LB-173 Web redesign

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,196 +1,6 @@
-/*//Basic React Native Stuff
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
-
-//Menu Stuff
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-//Vector Icons
-import Icon from 'react-native-vector-icons/Ionicons';
-
-//Importing project pages/tabs/content
-import RoommateFinder from './pages/RoommateFinder';
-import ContentArea from './pages/components/ContentArea';
-import ExpandableIcon from './pages/components/OverlayComponents/Menu/ExpandableIcon';
-
-//Importing standard stylesheet
-import PageLayouts from '@PageLayouts';
-
-function DashboardScreen(){
-	return(
-		<ContentArea
-			content={
-				<Text>TODO Dashboard Screen</Text>
-			}
-		/>
-	);
-}
-
-function ProfileScreen(){
-	return(
-		<ContentArea
-			content={
-				<Text>TODO Profile Screen</Text>
-			}
-		/>
-	);
-}
-
-function CalendarScreen(){
-	return(
-		<ContentArea
-			content={
-				<Text>TODO Calendar Screen</Text>
-			}
-		/>
-	);
-}
-
-function ChatScreen(){
-	return(
-		<ContentArea
-			content={
-				<Text>TODO Chat Screen</Text>
-			}
-		/>
-	);
-}
-
-function RoommateFinderScreen(){
-	return(
-		<ContentArea content={<RoommateFinder/>}/>
-	);
-}
-
-function RemindersScreen(){
-	return(
-		<ContentArea content={
-			<Text>TODO Reminder Screen</Text>
-		}/>
-	);
-}
-
-const Drawer = createDrawerNavigator();
-
-export default function App() {
-	//either have the sidebar width be 70 or 200
-	let sidebarWidth = 200;
-	
-	return (
-		<NavigationContainer>
-			<Drawer.Navigator 
-				initialRouteName="Dashboard"
-				screenOptions={{
-					drawerStyle: {
-						width: sidebarWidth
-					},
-				}}
-			>
-				<Drawer.Screen 
-					name="Dashboard"
-					component={DashboardScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="home"
-								label="Dashboard"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Drawer.Screen 
-					name="My Profile"
-					component={ProfileScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="person"
-								label="My Profile"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Drawer.Screen 
-					name="Roommate Finder"
-					component={RoommateFinderScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="search"
-								label="Roommate Finder"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Drawer.Screen 
-					name="Chat"
-					component={ChatScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="chatbubbles-outline"
-								label="Chat"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Drawer.Screen 
-					name="Calendar"
-					component={CalendarScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="calendar-sharp"
-								label="Calendar"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Drawer.Screen 
-					name="Reminders Screen" 
-					component={RemindersScreen}
-					options={{
-						drawerLabel: "",
-						drawerIcon: ({color}) => (
-							<ExpandableIcon
-								icon="checkbox-outline"
-								label="Reminders"
-								side="left"
-								width={sidebarWidth}
-								color={color}
-							/>
-						)
-					}}
-				/>
-			</Drawer.Navigator>
-		</NavigationContainer>
-	);
-}
-*/
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createTopButtonNavigator } from "./TopButtonNavigator";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import {
@@ -199,7 +9,7 @@ import {
   SafeAreaView,
   Text,
 } from "react-native";
-import { useContext, useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import * as SecureStore from "expo-secure-store";
 
 import { AuthContext } from "./context";
@@ -214,7 +24,7 @@ import AccountNavigation from "./pages/AccountNavigation/AccountNavigation";
 
 const isWeb = Platform.OS === "web";
 const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+const TopButton = createTopButtonNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -224,7 +34,7 @@ export default function App() {
   const [token, setUserToken] = useState(null);
   const ip = useMemo(() => {
     return {
-      myIp: "192.168.0.41",
+      myIp: "10.0.0.186",
     };
   }, []);
   const authContext = useMemo(() => {
@@ -396,107 +206,99 @@ export default function App() {
               />
             </Tab.Navigator>
           ) : (
-            <Drawer.Navigator
-              screenOptions={{
-                headerStyle: {
-                  backgroundColor: "white",
-                },
-                drawerStyle: {
-                  backgroundColor: "white",
-                },
-                drawerLabelStyle: {
-                  fontSize: 30,
-                  fontWeight: "bold",
-                  textAlign: "left",
-                  marginLeft: 0,
-                },
-                drawerItemStyle: {},
-                drawerType: dimensions.width >= 768 ? "permanent" : "front",
-                drawerActiveTintColor: "#28303F",
-                drawerActiveBackgroundColor: "#AED1FF",
-                drawerInactiveTintColor: "#444",
-                drawerInactiveBackgroundColor: "transparent",
-              }}
-            >
-              <Drawer.Screen
+            <TopButton.Navigator>
+              <TopButton.Screen
                 name="Dashboard"
                 component={DashBoardNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-home-outline"
                       size={25}
-                      color={focused ? "#333" : "#555"}
+                      color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#AFD2FF',
+                  offColor: '#DCEAFE'
                 }}
               />
-              <Drawer.Screen
-                name="Finder"
+              <TopButton.Screen
+                name="Roomate Finder"
                 component={FinderNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-search-outline"
                       size={25}
-                      color={focused ? "#333" : "#555"}
+                      color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#AFD2FF',
+                  offColor: '#DCEAFE'
                 }}
               />
-              <Drawer.Screen
+              <TopButton.Screen
                 name="Chat"
                 component={ChatNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-chatbubbles-outline"
                       size={25}
-                      color={focused ? "#333" : "#555"}
+                      color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#AFD2FF',
+                  offColor: '#DCEAFE'
                 }}
               />
-              <Drawer.Screen
+              <TopButton.Screen
                 name="Calendar"
                 component={CalendarNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-calendar-outline"
                       size={25}
-                      color={focused ? "#333" : "#555"}
+                      color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#AFD2FF',
+                  offColor: '#DCEAFE'
                 }}
               />
-              <Drawer.Screen
+              <TopButton.Screen
                 name="Reminders"
                 component={RemindersNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-notifications-outline"
                       size={25}
                       color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#AFD2FF',
+                  offColor: '#DCEAFE'
                 }}
               />
-              <Drawer.Screen
+              <TopButton.Screen
                 name="Account"
                 component={AccountNavigation}
                 options={{
-                  drawerIcon: ({ focused, size }) => (
+                  buttonIcon: ({ focused, size }) => (
                     <Ionicons
                       name="md-person-outline"
                       size={25}
                       color={focused ? "#333" : "#888"}
                     />
                   ),
+                  onColor: '#FFAAAA',
+                  offColor: '#FFDBDB',
+                  end: true
                 }}
               />
-            </Drawer.Navigator>
+            </TopButton.Navigator>
           )
         ) : (
           <Stack.Navigator
