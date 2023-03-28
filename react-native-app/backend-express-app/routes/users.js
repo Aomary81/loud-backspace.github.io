ab28546dfe915f23f42e2b2d9c7fb5e6ffd66105
Kenneth Munk <da.munkk@gmail.com>
2023-03-27 22:43:37 -0700

    LB-161 -- Resubmit for testing and integration

--- react-native-app/backend-express-app/routes/users.js
+++ react-native-app/backend-express-app/routes/users.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-//const MongoClient = require('mongodb').MongoClient;
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
@@ -15,30 +14,6 @@ router.route('/add').post((req, res) => {
     newUser.save()
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: '+ err));
-});
-
-router.route('/edit/desc').post((req,res) => {
-	const emailHash = req.params.emailHash;
-	const passwordHash = req.params.passwordHash;
-	/*
-	MongoClient.connect(url, function(err,db){
-		
-		if(err)throw err;
-		
-		//blocking collection name
-		const collection = db.collection('needs_name');
-		
-		collection.updateOne({email_hash: emailHash},{$set: {desc: req.body.desc}}, function(err,result){
-			
-			if(err) throw err;
-			
-			db.close();
-			
-			res.send("Description for " + emailHash + " updated");
-		});
-		
-	});
-	*/
 });
 
 module.exports = router;
--- react-native-app/backend-express-app/server.js
+++ react-native-app/backend-express-app/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const https = require("https");
 const fs = require("fs");
-const ip = "10.0.0.186";
+const ip = "localhost";
 
 require("dotenv").config();
 
