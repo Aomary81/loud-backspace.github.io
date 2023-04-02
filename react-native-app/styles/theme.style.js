8277e1c260b0057941d8e83ea9b6558c62535862
Biscuit <rjfarruggia@csus.edu>
2023-04-02 03:09:33 -0700

    (resubmission) dashboard front + back

--- react-native-app/styles/theme.style.js
+++ react-native-app/styles/theme.style.js
@@ -1,8 +1,10 @@
 import { Appearance } from "react-native";
 
 export default {
-    BACKGROUND_COLOR: Appearance.getColorScheme() === 'dark' ? '#333' : 'white',
-    CONTAINER_COLOR: Appearance.getColorScheme() === 'dark' ? '#969696' : '#F3F3F3',
-    CONTENT_MODULE_COLOR: Appearance.getColorScheme() === 'dark' ? '#787878' : '#E1E1E1',
-    INPUT_COLOR: Appearance.getColorScheme() === 'dark' ? '#B4B4B4' : '#e9e9e9',
+    BACKGROUND_COLOR: Appearance.getColorScheme() === 'dark' ? '#222222' : 'white',
+    CONTAINER_COLOR: Appearance.getColorScheme() === 'dark' ? '#303030' : '#F3F3F3',
+    CONTENT_MODULE_COLOR: Appearance.getColorScheme() === 'dark' ? '#444' : '#E1E1E1',
+    INPUT_COLOR: Appearance.getColorScheme() === 'dark' ? '#454545' : '#e9e9e9',
+    INPUT_TEXT_COLOR: Appearance.getColorScheme() === 'dark' ? '#eeeeee' : 'black',
+    TEXT_COLOR: Appearance.getColorScheme() === 'dark' ? '#eeeeee' : 'black'
 };
