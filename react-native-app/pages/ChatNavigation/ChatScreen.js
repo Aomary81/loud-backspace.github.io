4a1943f34f6a426bb662fab3fd9dc66929f87762
Kenneth Munk <contact.me@kenmunk.com>
2023-04-03 01:04:17 -0700

    LB-176 & LB-177 -- repurposing the de-prioritized chat feature as a testing field

--- react-native-app/pages/ChatNavigation/ChatScreen.js
+++ react-native-app/pages/ChatNavigation/ChatScreen.js
@@ -1,34 +1,115 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import React, { useState, useEffect, useContext  } from "react";
+import { AuthContext } from "../../context";
+
 import theme from '../../styles/theme.style'
 
 function ChatScreen() {
-    return (
-      <SafeAreaView style={styles.background}>
-        <StatusBar style="auto" />
-        <View style={styles.container}>
-          <Text>ChatScreen</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
-  export default ChatScreen;
+	
 
-  const styles = StyleSheet.create({
-    background: {
-      flex: 1,
-      backgroundColor: theme.BACKGROUND_COLOR,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    container:{
-      flex: 1,
-      width: '100%',
-      backgroundColor: theme.CONTAINER_COLOR,
-      borderRadius: 10,
-      borderWidth: 5,
-      borderColor: theme.CONTAINER_COLOR,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-  });
+	const { myIp } = useContext(AuthContext).ip;
+	const { token } = useContext(AuthContext);
+
+	const [success, setSuccess] = useState(false);
+
+	useEffect(() => { 
+		const endpoint = "/household/get-household"
+		const api_address = "http://" + myIp + ":3000"+endpoint
+		const userData = fetch(api_address, {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				token: token,
+			}),
+		})
+		.then(response => {
+			console.log(response);
+			return(response.json());
+		})
+		.then(data => {
+			
+			console.log("Successfully loaded test data from "+api_address)
+			console.log(data);
+		});
+		
+		const endpoint2 = "/get/userById/idinBody"
+		const api_address2 = "http://" + myIp + ":3000"+endpoint2
+		console.log("Attempting to get data from " + api_address2);
+		const userData2 = fetch(api_address2, {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				token: token,
+				userID: "640e8041e7fc5b8072e23e31"
+			}),
+		})
+		.then(response => {
+			console.log(response);
+			return(response.json());
+		})
+		.then(data => {
+			
+			console.log("Successfully loaded test data from "+api_address)
+			console.log(data);
+		});
+		
+		const endpoint3 = "/get/roommates"
+		const api_address3 = "http://" + myIp + ":3000"+endpoint3
+		console.log("Attempting to get data from " + api_address3);
+		const userData3 = fetch(api_address3, {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				token: token,
+			}),
+		})
+		.then(response => {
+			console.log(response);
+			return(response.json());
+		})
+		.then(data => {
+			
+			console.log("Successfully loaded test data from "+api_address)
+			console.log(data);
+		});
+	}, []);
+	
+	return (
+		<SafeAreaView style={styles.background}>
+			<StatusBar style="auto" />
+			<View style={styles.container}>
+				<Text>ChatScreen</Text>
+			</View>
+		</SafeAreaView>
+	);
+}
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+	background: {
+		flex: 1,
+		backgroundColor: theme.BACKGROUND_COLOR,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	container:{
+		flex: 1,
+		width: '100%',
+		backgroundColor: theme.CONTAINER_COLOR,
+		borderRadius: 10,
+		borderWidth: 5,
+		borderColor: theme.CONTAINER_COLOR,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
